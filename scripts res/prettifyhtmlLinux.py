ac31e0e7dd412a9e133c3e2592f4b315a6726c99
TheOriginalRedHood <ninjastrike454@gmail.com>
2017-12-10 19:59:37 -0800

    python prettify fixed

--- scripts res/prettifyhtmlLinux.py
+++ scripts res/prettifyhtmlLinux.py
@@ -1,7 +1,7 @@
 import os
 os.system("sudo pip install --upgrade beautifulsoup4")
 from bs4 import BeautifulSoup as BS
-html = raw_input("HTML file? \t")
+html = input("HTML file? \t")
 f = open(html,"r+")
 soup = BS(f.read(), "html.parser")
 f.seek(0,0)
